31249ad78676c2254e880b44902e2bde06a46d5f
itsmefminsaf <itsmefminsaf@gmail.com>
2025-10-12 00:43:21 +0530

    Refactor layout and Nav components for improved styling and structure

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -6,29 +6,35 @@ import { useState } from "react";
 import { BiLogOut } from "react-icons/bi";
 import logo from "@/assets/snape.svg";
 import { CgClose } from "react-icons/cg";
+import { IoIosArrowDown } from "react-icons/io";
 
 const Nav = ({ name, picture }: { name: string; picture: string }) => {
   const [showProfileDropDown, setShowProfileDropDown] = useState(false);
 
   return (
-    <nav className="mx-auto mt-3 flex w-[98vw] items-center justify-between rounded-2xl bg-neutral-950 px-5 py-3 text-white shadow-2xl">
-      <div className="flex items-center gap-5">
+    <nav className="flex items-center justify-between border-b-2 border-b-neutral-800 bg-neutral-950/50 text-white backdrop-blur-2xl">
+      <div className="flex items-center gap-5 border-r-2 border-r-neutral-800 p-5">
         <Image src={logo} alt="Snape AI" width={50} height={50} />
         <hr className="h-7 w-0.5 rotate-12 rounded-full bg-white" />
         <h1 className="text-2xl">Dashboard</h1>
       </div>
 
-      <div className="relative">
-        <Image
-          src={picture}
-          alt="user's profile"
-          width={40}
-          height={40}
-          className="rounded-full"
+      <div className="relative border-l-2 border-l-neutral-800 p-6">
+        <div
           onClick={() => setShowProfileDropDown(true)}
-        />
+          className="flex items-center justify-center gap-3"
+        >
+          <Image
+            src={picture}
+            alt="user's profile"
+            width={40}
+            height={40}
+            className="rounded-full"
+          />
+          <IoIosArrowDown />
+        </div>
         {showProfileDropDown && (
-          <div className="absolute top-16 right-0 min-w-52 space-y-2 rounded-xl bg-neutral-900 px-5 py-3 shadow-2xl">
+          <div className="absolute top-[100%] right-0 min-w-52 space-y-2 border-2 border-neutral-800 bg-neutral-950/50 px-5 py-3 shadow-2xl backdrop-blur-2xl">
             <button
               className="flex w-full items-center justify-between gap-3"
               onClick={() => setShowProfileDropDown(false)}
