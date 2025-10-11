66caa9be37f1dd9b23333584fd2e5181d5abfec7
itsmefminsaf <itsmefminsaf@gmail.com>
2025-10-11 20:45:02 +0530

    Add MongoDB integration and implement workspace retrieval functionality

---
+++ src/actions/getWorkspaces.ts
@@ -0,0 +1,15 @@
+"use server";
+
+import connectDB from "@/lib/db";
+
+const getWorkspaces = async (email: string) => {
+  try {
+    const workspaceCollection = await connectDB("", "workspaces");
+    const workspaces = await workspaceCollection.find({ email }).toArray();
+    return workspaces;
+  } catch (error) {
+    throw new Error(`Error getting workspaces: ${error}`);
+  }
+};
+
+export default getWorkspaces;
