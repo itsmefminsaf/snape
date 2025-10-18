e3de77708008f7673d4dba810e20406c2de92bbf
itsmefminsaf <itsmefminsaf@gmail.com>
2025-10-18 15:30:54 +0530

    feat: created a minimal landing page

--- src/components/LandingPage.tsx
+++ src/components/LandingPage.tsx
@@ -1,10 +1,21 @@
 import Link from "next/link";
+import { BiArrowToRight } from "react-icons/bi";
 
 const LandingPage = () => {
   return (
-    <>
-      <h1>Welcome to Snape</h1> <Link href="/auth/login">Login</Link>
-    </>
+    <main className="center h-screen w-screen flex-col gap-3 text-white">
+      <h1 className="text-xl font-bold">Welcome to Snape</h1>
+      <h2 className="text-lg text-neutral-400">
+        - Great power comes with, great responsibly -
+      </h2>
+      <Link
+        href="/auth/login"
+        className="center gap-2 hover:scale-105 active:scale-95 duration-300 bg-white px-3 py-2 text-2xl font-extrabold text-neutral-950"
+      >
+        <BiArrowToRight />
+        Start using for free
+      </Link>
+    </main>
   );
 };
 
