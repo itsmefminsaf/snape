4f5dcec87368cbad67d2072fc82949d0e0f62a23
itsmefminsaf <itsmefminsaf@gmail.com>
2025-10-18 14:25:32 +0530

    undo vercel deployment problem detection

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -2,17 +2,17 @@ import Nav from "./Nav";
 import { User } from "@auth0/nextjs-auth0/types";
 import AddWorkspaceForm from "./AddWorkspaceForm";
 import UserMenu from "./UserMenu";
-// import Workspaces from "./Workspaces";
+import Workspaces from "./Workspaces";
 import { getWorkspaceList } from "@/actions/getWorkspaces";
 
 const Dashboard = async ({ user }: { user: User }) => {
-  // const workspaces = await getWorkspaceList(user.email || "");
+  const workspaces = await getWorkspaceList(user.email || "");
   return (
     <>
       <Nav picture={user.picture || ""} />
       <UserMenu name={user.name || "Unknown"} />
       <AddWorkspaceForm email={user.email || ""} />
-      {/* <Workspaces workspaces={workspaces} /> */}
+      <Workspaces workspaces={workspaces} />
     </>
   );
 };
